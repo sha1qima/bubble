d7dc933fedb0c43ba7d2df8f614aba2dcc716ac3
sha1qima <x00217230109@njit.edu.cn>
2024-03-04 22:36:28 +0800

    SUBMITTED

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main { 
+    public static void main (String[] args) throws Exception {
+        int[] array1 = {38, 65, 3, 17, 2};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.bubbleSort(array1);
+    }
+}
